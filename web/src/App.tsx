38226e4e8ddbd9b2a608807c5150c2772f464751
Pedro Henrique Correa Mota da Silva <pedrohcms2018@gmail.com>
2020-06-03 13:02:57 -0300

    Created Home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+import Home from "./pages/Home";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
-      </header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
