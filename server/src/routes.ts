8a8bdc374d957223846fc9de40ab5009f173b8d5
Pedro Henrique Correa Mota da Silva <pedrohcms2018@gmail.com>
2020-06-02 22:39:54 -0300

    Created route that find a point by id

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,59 +1,13 @@
 import express from "express";
-import knex from "./database/connection";
+
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
 
 const routes = express.Router();
 
-routes.get("/items", async (req, res) => {
-  const items = await knex("items").select("*");
+routes.get("/items", ItemsController.index);
 
-  const serializedItems = items.map((item) => {
-    return {
-      id: item.id,
-      name: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  });
-
-  return res.json(serializedItems);
-});
-
-routes.post("/points", async (req, res) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items,
-  } = req.body;
-
-  const trx = await knex.transaction();
-
-  const insertedIds = await trx("points").insert({
-    image: "image-fake",
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-  });
-
-  const point_id = insertedIds[0];
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id,
-    };
-  });
-
-  await trx("item_point").insert(pointItems);
-
-  return res.json({ sucess: true });
-});
+routes.get("/points/:id", PointsController.show);
+routes.post("/points", PointsController.create);
 
 export default routes;
