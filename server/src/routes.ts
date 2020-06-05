6dca0dc98819526a7dca18593fb6ecb4a25374d7
Pedro Henrique Correa Mota da Silva <pedrohcms2018@gmail.com>
2020-06-05 18:38:47 -0300

    Added validation on backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import multer from "multer";
+import { celebrate, Joi } from "celebrate";
+
 import multerConfig from "./config/multer";
 
 import PointsController from "./controllers/PointsController";
@@ -13,6 +15,27 @@ routes.get("/items", ItemsController.index);
 routes.get("/points", PointsController.index);
 routes.get("/points/:id", PointsController.show);
 
-routes.post("/points", upload.single("image"), PointsController.create);
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  PointsController.create
+);
 
 export default routes;
